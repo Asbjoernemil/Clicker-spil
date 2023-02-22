fc490a5004b00dd682209c91b96f418ad76cf09e
Asbjoernemil <asbjoernemil24@gmail.com>
2023-02-22 17:14:06 +0100

    kul og diamanter
    mangler at få liv til at tælle ned

--- animationer.js
+++ animationer.js
@@ -1,15 +1,101 @@
+"use strict";
+let lives = 3;
+let score = 0;
 window.addEventListener("load", start);
+
+/* Animationen startes*/
 
 function start() {
   console.log("Spillet starter");
+  document.querySelector("#gold_container").classList.add("falling2");
+  document.querySelector("#coal_container").classList.add("falling3");
+
+  document
+    .querySelector("#gold_container")
+    .addEventListener("mousedown", clickGold);
+
+  document
+    .querySelector("#coal_container")
+    .addEventListener("mousedown", clickCoal);
+}
+
+/* GULD */
+function clickGold() {
+  console.log("clicked");
+  /* Forhindre at der kan gentages clicks */
+  document
+    .querySelector("#gold_container")
+    .removeEventListener("mousedown", clickGold);
+
+  /* Animation når der clickes på elementet */
+  document.querySelector("#gold_container").classList.add("paused");
+  document.querySelector("#gold_sprite").classList.add("zoom_out");
+  document.querySelector("#gold_splash").classList.add("splash");
+
+  document
+    .querySelector("#gold_splash")
+    .addEventListener("animationend", goldGone);
+}
+
+function goldGone() {
+  /* animation restartes*/
+
+  document.querySelector("#gold_container").classList.remove("falling2");
+  document.querySelector("#gold_container").classList.remove("paused");
+  document.querySelector("#gold_container").offsetWidth;
+  document.querySelector("#gold_container").classList.add("falling2");
+  document.querySelector("#gold_sprite").classList.remove("zoom_out");
+  document.querySelector("#gold_splash").classList.remove("splash");
+
+  /* Vi kan clicke igen*/
   document
     .querySelector("#gold_container")
     .addEventListener("mousedown", clickGold);
 }
-function clickGold() {
-  console.log("clicked");
-  document.querySelector("#gold_container").classList.add("paused");
+
+/*KUL*/
+function clickCoal() {
+  console.log("clicked2");
+  /* Forhindre at der kan gentages clicks */
   document
-    .querySelector("#gold_container")
-    .removeEventListener("mousedown", clickGold);
+    .querySelector("#coal_container")
+    .removeEventListener("mousedown", clickCoal);
+
+  /* Animation når der clickes på elementet */
+  document.querySelector("#coal_container").classList.add("paused");
+  document.querySelector("#coal_sprite").classList.add("zoom_out");
+  document.querySelector("#coal_splash").classList.add("splash");
+
+  document
+    .querySelector("#coal_splash")
+    .addEventListener("animationend", coalGone);
+  life();
+}
+
+/* Der mistes liv ved at trykke på kul */
+function life() {
+  displaylife();
+  lives--;
+}
+
+function displaylife() {
+  console.log("heart broken");
+  document.querySelector("#heart" + lives).classList.remove("active_heart");
+  document.querySelector("#heart" + lives).classList.add("broken_heart");
+}
+
+function coalGone() {
+  /* animation restartes*/
+
+  document.querySelector("#coal_container").classList.remove("falling3");
+  document.querySelector("#coal_container").classList.remove("paused");
+  document.querySelector("#coal_container").offsetWidth;
+  document.querySelector("#coal_container").classList.add("falling3");
+  document.querySelector("#coal_sprite").classList.remove("zoom_out");
+  document.querySelector("#coal_splash").classList.remove("splash");
+
+  /* Vi kan clicke igen*/
+  document
+    .querySelector("#coal_container")
+    .addEventListener("mousedown", clickCoal);
 }
